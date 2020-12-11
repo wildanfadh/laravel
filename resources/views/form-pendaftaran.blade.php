b488243632c52ef5e1e81a280d0fd9ae8ef645b0
wildanfadh <75764845+wildanfadh@users.noreply.github.com>
2020-12-11 11:36:37 +0700

    Add files via upload

--- resources/views/form-pendaftaran.blade.php
+++ resources/views/form-pendaftaran.blade.php
@@ -17,6 +17,53 @@
     <div class="row">
         <div class="col-md-8 col-xl-6">
         <h1>Pendaftaran Mahasiswa</h1>
+        <hr>
+        <form action="{{url('/proses-form')}}" method="GET">
+            <div class="form-group">
+                <label for="nim">NIM</label>
+                <input type="text" class="form-control" id="nim" name="nim">
+            </div>
+
+            <div class="form-group">
+                <label for="nama">Nama Lengkap</label>
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" name="email">
+            </div>
+
+            <div class="form-group">
+                <label>Jenis Kelamin</label>
+                <div class="form-check form-check-inline">
+                    <input type="radio" class="form-check-input" id="laki_laki" name="jenis_kelamin" value="L">
+                    <label class="form-check-label" for="laki_laki">Laki-laki</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input type="radio" class="form-check-input" id="perempuan" name="jenis_kelamin" value="P">
+                    <label class="form-check-label" for="perempuan">Perempuan</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-control" name="jurusan" id="jurusan"> 
+                    <option value="Teknik Informatika">Teknik Informatika</option>
+                    <option value="Sistem Informasi">Sistem Informasi</option>
+                    <option value="Ilmu Komputer">Ilmu Komputer</option>
+                    <option value="Teknik Komputer">Teknik Komputer</option>
+                    <option value="Teknik Telekomunikasi">Teknik Telekomunikasi</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <textarea class="form-control" id="alamat" rows="3" name="alamat"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary mb-2">Daftar</button>
+        </form>
         </div>
     </div>
 </div>
