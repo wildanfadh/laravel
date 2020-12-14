2273e871e33ed1a15fb236256b4aafd5a045d70c
wildanfadh <75764845+wildanfadh@users.noreply.github.com>
2020-12-14 15:48:46 +0700

    upload laravel kuy

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\DaftarMahasiswa;
 use Illuminate\Support\Facades\DB;
 use App\Models\Mahasiswa;
 
@@ -479,7 +481,66 @@ class MahasiswaController extends Controller
     {
         return view('form-pendaftaran');
     }
+
+    public function prosesForm(Request $request)
+    {
+        $validateData = $request->validate([
+            'nim' => 'required|size:8',
+            'nama' => 'required|min:3|max:50',
+            'email' => 'required|email',
+            'jenis_kelamin' => 'required|in:P,L',
+            'jurusan' => 'required',
+            'alamat' => '',
+        ]);
+
+        dump($validateData);
+
+        echo $validateData['nim'];             echo "<br>";
+        echo $validateData['nama'];            echo "<br>";
+        echo $validateData['email'];           echo "<br>";
+        echo $validateData['jenis_kelamin'];   echo "<br>";
+        echo $validateData['jurusan'];         echo "<br>";
+        echo $validateData['alamat'];          
+    }
+
+    public function prosesFormValidator(Request $request)
+    {
+        $rules = [
+            'nim'           => 'required|size:8',
+            'nama'          => 'required|min:3|max:50',
+            'email'         => 'required|email',
+            'jenis_kelamin' => 'required|in:P,L',
+            'alamat'        => '',
+        ];
+
+        $error_message = [
+            'required'  => ':attribute wajib diisi.',
+            'size'      => ':attribute harus berukuran :size  karakter.',
+            'max'       => ':attribute maksimal berisi :max karakter.',
+            'min'       => ':attribute minimal berisi :min karakter',
+            'email'     => ':attribute harus diisi dengan alamat email yang valid',
+            'in'        => ':attribute yang dipilih tidak valid.',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $error_message);
+
+        if ($validator->fails()) {
+            return redirect('/')->withErrors($validator)->withInput();
+        } else {
+            echo $request->nim;            echo "<br>";
+            echo $request->nama;           echo "<br>";
+            echo $request->email;          echo "<br>";
+            echo $request->jenis_kelamin;  echo "<br>";
+            echo $request->jurusan;        echo "<br>";
+            echo $request->alamat;         echo "<br>";
+        }
+    }
        
+    public function prosesFormRequest(DaftarMahasiswa $request)
+    {
+        $validateData = $request->validated();
+        dump($validateData);
+    }
     
 
 }
