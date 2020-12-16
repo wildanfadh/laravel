d914966ed119628f2611f3035098d77ec04de569
wildanfadh <75764845+wildanfadh@users.noreply.github.com>
2020-12-16 11:06:25 +0700

    view learing laravel

--- resources/views/form-pendaftaran.blade.php
+++ resources/views/form-pendaftaran.blade.php
@@ -16,14 +16,14 @@
 <div class="container pt-4 bg-white">
     <div class="row">
         <div class="col-md-8 col-xl-6">
-        <h1>Pendaftaran Mahasiswa</h1>
+        <h1>{{ __('form.judul') }}</h1>
         <hr>
 
 
-        <form action="{{url('/proses-form-request')}}" method="POST">
+        <form action="{{url('/proses-form')}}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="nim">NIM</label>
+                <label for="nim">{{ __('form.input.nim') }}</label>
                 <input type="text" class="form-control @error('nim') is-invalid @enderror" id="nim" name="nim" value="{{ old('nim') }}">
                 @error('nim')
                 <div class="text-danger">{{ $message }}</div>
@@ -31,7 +31,7 @@
             </div>
 
             <div class="form-group">
-                <label for="nama">Nama Lengkap</label>
+                <label for="nama">{{ __('form.input.nama_lengkap') }}</label>
                 <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" value="{{ old('nama') }}">
                 @error('nama')
                 <div class="text-danger">{{ $message }}</div>
@@ -39,7 +39,7 @@
             </div>
 
             <div class="form-group">
-                <label for="email">Email</label>
+                <label for="email">{{ __('form.input.email') }}</label>
                 <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
                 @error('email')
                 <div class="text-danger">{{ $message }}</div>
@@ -47,14 +47,14 @@
             </div>
 
             <div class="form-group">
-                <label>Jenis Kelamin</label>
+                <label>{{ __('form.input.jenis_kelamin') }}</label>
                 <div class="form-check form-check-inline">
                     <input type="radio" class="form-check-input" id="laki_laki" name="jenis_kelamin" value="L" {{ old('jenis_kelamin')=='L' ? 'chacked':'' }}>
-                    <label class="form-check-label" for="laki_laki">Laki-laki</label>
+                    <label class="form-check-label" for="laki_laki">{{ __('form.input.pilihan_jenis_kelamin.laki_laki') }}</label>
                 </div>
                 <div class="form-check form-check-inline">
                     <input type="radio" class="form-check-input" id="perempuan" name="jenis_kelamin" value="P" {{ old('jenis_kelamin')=='P' ? 'chacked':'' }}>
-                    <label class="form-check-label" for="perempuan">Perempuan</label>
+                    <label class="form-check-label" for="perempuan">{{ __('form.input.pilihan_jenis_kelamin.perempuan') }}</label>
                 </div>
                 @error('jenis_kelamin')
                 <div class="text-danger">{{ $message }}</div>
@@ -62,7 +62,7 @@
             </div>
 
             <div class="form-group">
-                <label for="jurusan">Jurusan</label>
+                <label for="jurusan">{{ __('form.input.jurusan') }}</label>
                 <select class="form-control" name="jurusan" id="jurusan"> 
                     <option value="Teknik Informatika" {{ old('jurusan')=='Teknik Informatika' ? 'selected':'' }}>Teknik Informatika</option>
                     <option value="Sistem Informasi" {{ old('jurusan')=='Sistem Informasi' ? 'selected':'' }}>Sistem Informasi</option>
@@ -76,11 +76,11 @@
             </div>
 
             <div class="form-group">
-                <label for="alamat">Alamat</label>
+                <label for="alamat">{{ __('form.input.alamat') }}</label>
                 <textarea class="form-control" id="alamat" rows="3" name="alamat">{{ old('alamat') }}</textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary mb-2">Daftar</button>
+            <button type="submit" class="btn btn-primary mb-2">{{ __('form.input.tombol') }}</button>
         </form>
         </div>
     </div>
